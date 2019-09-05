3596100b80fff8dc867495d68927b2caa27178b6
Cade Covington <ccovington233@gmail.com>
2019-09-05 17:04:31 -0500

    Initial commit

---
+++ airDistance.c
@@ -0,0 +1,27 @@
+/*
+ * Author: Cade Covington
+ * Date: 9/5/19
+ *
+ * This program will calculate the distance between
+ * two points of latitude and longitude.
+ *
+ */
+
+ #include<stdlib.h>
+ #include<stdio.h>
+ #include<math.h>
+
+int main(int argc, char **argv) {
+
+  // declare variables
+  double longitude_A;
+  double latitude_A;
+  double longitude_B;
+  double latitude_B;
+
+printf("Please enter the longitude and latitude of point A: \n");
+scanf("%lf,%lf", longitude_A, latitude_A);
+
+printf("Please enter the longitude and latitude of point B: \n");
+scanf("%lf,%lf", longitude_B, latitude_B);
+}
